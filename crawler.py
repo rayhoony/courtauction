25154d589072734fe0c05ce5258f0bc87c4d028f
rayhoony <111360432+rayhoony@users.noreply.github.com>
2026-04-20 22:16:14 +0900

    Update crawler.py
    headless를 true로

--- crawler.py
+++ crawler.py
@@ -7,7 +7,7 @@ from playwright.async_api import async_playwright
 
 async def main():
     async with async_playwright() as p:
-        browser = await p.chromium.launch(headless=False)  # headless=True로 변경하면 창 없이 실행
+        browser = await p.chromium.launch(headless=True)  # headless=True로 변경하면 창 없이 실행
         context = await browser.new_context(
             user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
         )
